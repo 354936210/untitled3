8d082b80159e7cc2d7ceef8b5391995cc443ba27
王超 <354936210@qq.com>
2020-03-16 02:38:33 +0800

    test

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import VideoPlayer from 'vue-video-player'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
+Vue.use(VideoPlayer)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <iframe src="//player.bilibili.com/player.html?aid=93304618&cid=159300188&page=1"
+            allowfullscreen="true"></iframe>
+  </div>
+</template>
+<style>
+iframe{
+height: 359px;
+  width: 638px;
+}
+</style>
